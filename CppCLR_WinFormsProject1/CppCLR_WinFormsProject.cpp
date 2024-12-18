f0d005efaf0b9428223cddae7e275287cfd10ea9
Scott Rudrow <SERudrow@student.fullsail.edu>
2024-12-18 14:44:09 -0700

    Connected to SDL

--- CppCLR_WinFormsProject1/CppCLR_WinFormsProject.cpp
+++ CppCLR_WinFormsProject1/CppCLR_WinFormsProject.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "Form1.h"
+#include <SDL.h>
+
 using namespace System;
 
 
@@ -8,10 +10,17 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-int main()
+int main(int argc, char* argv[]); 
+
+void myFunction()
+
 {
-  Application::EnableVisualStyles();
-  Application::SetCompatibleTextRenderingDefault(false);
-  Application::Run(gcnew CppCLRWinFormsProject::Form1());
-  return 0;
+
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew CppCLRWinFormsProject::Form1());
+
+	//SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Title", "Discription", NULL);
+	//return 0;
+
 }
--- CppCLR_WinFormsProject1/Form1.h
+++ CppCLR_WinFormsProject1/Form1.h
@@ -68,9 +68,10 @@ namespace CppCLRWinFormsProject {
 			this->label1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(238, 150);
+			this->label1->Location = System::Drawing::Point(159, 98);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(128, 20);
+			this->label1->Size = System::Drawing::Size(85, 13);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"A Cannon Game";
 			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
@@ -85,39 +86,44 @@ namespace CppCLRWinFormsProject {
 			this->tableLayoutPanel1->Controls->Add(this->button1, 0, 0);
 			this->tableLayoutPanel1->Controls->Add(this->button2, 0, 1);
 			this->tableLayoutPanel1->Controls->Add(this->button3, 0, 2);
-			this->tableLayoutPanel1->Location = System::Drawing::Point(201, 207);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(134, 135);
+			this->tableLayoutPanel1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 3;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 43.58974F)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 56.41026F)));
-			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 47)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(207, 153);
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 31)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(138, 99);
 			this->tableLayoutPanel1->TabIndex = 1;
 			this->tableLayoutPanel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Form1::tableLayoutPanel1_Paint);
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(3, 3);
+			this->button1->Location = System::Drawing::Point(2, 2);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(201, 40);
+			this->button1->Size = System::Drawing::Size(134, 25);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"PLAY";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(3, 49);
+			this->button2->Location = System::Drawing::Point(2, 31);
+			this->button2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(201, 53);
+			this->button2->Size = System::Drawing::Size(134, 34);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"OPTION";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(3, 108);
+			this->button3->Location = System::Drawing::Point(2, 69);
+			this->button3->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(201, 42);
+			this->button3->Size = System::Drawing::Size(134, 27);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"EXIT";
 			this->button3->UseVisualStyleBackColor = true;
@@ -125,11 +131,12 @@ namespace CppCLRWinFormsProject {
 			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(595, 555);
+			this->ClientSize = System::Drawing::Size(397, 361);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->tableLayoutPanel1->ResumeLayout(false);
@@ -146,7 +153,10 @@ namespace CppCLRWinFormsProject {
 	}
 	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
 		this->Close();
-
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Title", "Discription", NULL);
+		return 0;
 	}
 };
 }
